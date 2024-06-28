a49802345303166530aa32accc196af5b11d14e2
Meghanath Reddy M C <116367208+MeghanathMC@users.noreply.github.com>
2024-06-28 19:03:31 +0530

    final commit

--- task2/task2.js
+++ task2/task2.js
@@ -1,7 +1,25 @@
 let slideNumber = 1;
 let maxSlides = 4;
 
-function prevSlide() {}
+function prevSlide() {
+  slideNumber -= 1;
+  if (slideNumber < 1) {
+    slideNumber = maxSlides;
+  }
+  document.getElementById("output").src =
+    "./images/img" + slideNumber + ".jpeg";
+  output.classList.add("animate");
+  setTimeout(()=>{
+    output.classList.remove("animate");
+  },2000);
+  if(slideNumber===1){
+    document.getElementById("left-arrow").setAttribute("disabled",true);
+  }
+  
+
+  }
+
+
 
 function nextSlide() {
   slideNumber += 1;
@@ -10,4 +28,13 @@ function nextSlide() {
   }
   document.getElementById("output").src =
     "./images/img" + slideNumber + ".jpeg";
+    output.classList.add("animate");
+  setTimeout(()=>{
+    output.classList.remove("animate");
+  },2000);
+  if(slideNumber===4){
+    document.getElementById("right-arrow").setAttribute("disabled",true);
+  }
+  
+
 }
